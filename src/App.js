e68e57d2025c71cc412e9664dcb077d00ab0519e
Sourin <iamsourinsarkar@gmail.com>
2023-02-09 14:57:54 +0530

    Direct render done now

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import Numbers from './components/Numbers'
 function App() {
     return <div>
         <h1>Numbers List</h1>
-        <Numbers />
+        <Numbers numbers = {[1,2,3,4,5]} />
+        <Numbers numbers = {[1,2,3,4,5,6]} />
+        <Numbers numbers = {[1,2,3,4,5,6,7]} />
+        <Numbers numbers = {[1,2,3,4,5,6,7,8]} />
+        <Numbers numbers = {[1,2,3,4,5,6,7,8,9]} />
     </div>
 }
 
--- src/components/Numbers.js
+++ src/components/Numbers.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
-export default function Numbers() {
-    const numbers = [1,2,3,4,5]
+export default function Numbers({numbers}) {
     const Numbers = numbers.map((x) => <li>{x}</li>)
   return (
     <div>
