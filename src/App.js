ee3a34bd9c02217f20b3cb99e9dec70a329b3233
Sourin <iamsourinsarkar@gmail.com>
2023-02-20 19:53:23 +0530

    Different ways to show the array

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 // import Fskills from './components/Fskills'
 // import Numbers from './components/Numbers'
 // import FStates from './components/FStates'
+import People from './components/People'
 
 
 function App() {
 
-    const anything = [
-        ["Sourin", "Kangana", "Sheetal", "Aastha"],
-        ["C++", "Java","JavaScript", "Rust"]
+    // const anything = [
+    //     ["Sourin", "Kangana", "Sheetal", "Aastha"],
+    //     ["C++", "Java","JavaScript", "Rust"]
+    // ]
+
+    // const student = [
+    //     ["Name", "Subject", "Marks"],
+    //     ["Sourin", "React", "100"],
+    //     ["Sheetal", "Vue", "99"]
+    // ]
+    const people = [
+        {
+            id : 1,
+            name : "Sourin",
+            skills : ["C++", "Java","JavaScript", "Rust"],
+            date : new Date(2021, 2, 26)
+        },
+        {
+            id : 2,
+            name : "ABC",
+            skills : ["C++", "Python","Ruby", "Erlang"]
+        },
+        {
+            id : 3,
+            name : "XYZ",
+            skills : ["Elixr", "Java","Golang", "Rust"]
+        },
+        {
+            id : 4,
+            name : "MNO",
+            skills : ["R", "Java", "C++", "HTML"]
+        }
     ]
+
     return (
     <div>
         {/* <h1>Numbers List</h1>
@@ -21,10 +52,10 @@ function App() {
         {/* <FStates />  */}
         {/* <Numbers numbers = {[1,2,3,4,5]} />*/}
 
-        {anything.map((items, index) => {
+        {/* {anything.map((items, index) => {
             return (
                 <div>
-                    {/* <li>{items}</li> */}
+                    <li>{items}</li>
 
                     <ol key = {index}>
                         {items.map((subitems, subindex) => {
@@ -33,7 +64,45 @@ function App() {
                     </ol>
                 </div>
             )
+        })} */}
+
+        {/* <table>
+            <thead>
+                <tr>
+                    {student[0].map((item,index) => {
+                        return <th key = {index}>{item}</th>
+                    })}
+                </tr>
+            </thead>
+            <tbody>
+                {student.slice(1).map((items, index) => {
+                    return (
+                        <tr>
+                            <td key = {index}>{items[0]}</td>
+                            <td key = {index}>{items[1]}</td>
+                            <td key = {index}>{items[2]}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table> */}
+
+        {people.map((person, index) => {
+            return (
+                <div>
+                    <h1>Name : {person.name}</h1>             
+                </div>
+            )
         })}
+
+        {/* <People id = {people.id} name = {people.name} /> */}
+
+        <People 
+        id = {people[0].id}
+        name = {people[0].name}
+        date = {people[0].date}
+        />
+
     </div>
     )
 }
